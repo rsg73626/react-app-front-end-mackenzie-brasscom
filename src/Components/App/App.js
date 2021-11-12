52212bdb9614d1dba4b8492ecc9340336f632914
Renan Germano <renangermano@MacBook-Pro-de-Renan-2.local>
2021-11-12 15:36:21 -0300

    Criando componentes Pessoa e ElementoQuimico.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -32,6 +32,26 @@ const doisParagrafosComFragment = (
     </>
 );
 
+function Pessoa({nome, sobrenome, idade}) {
+    return (
+        <ul>
+            <li>Nome: {nome}.</li>
+            <li>Sobrenome: {sobrenome}.</li>
+            <li>Idade: {idade}.</li>
+        </ul>
+    );
+}
+
+function ElementoQuimico(props) {
+    return (
+        <p className="elemento">
+            <span>{props.nmr}</span>
+            <span>{props.simbulo}</span>
+            <span>{props.nome}</span>
+        </p>
+    );
+}
+
 function App() {
     return (
         <>
@@ -57,6 +77,38 @@ function App() {
                 <article>
                     <h2>Componente referenciando outro componente.</h2>
                     <Garagem />
+                </article>
+                <article>
+                    <h2>Prática (primeiro componente)</h2>
+                    <ol>
+                        <li>Criar um componente pessoa.</li>
+                        <li>O componente deve receber a propriedade Nome.</li>
+                        <li>O componente deve receber a propriedade Sobrenome.</li>
+                        <li>O componente deve receber a propriedade Idade.</li>
+                        <li>O componente deve criar (retornar) uma 
+                            ul com cada li mostrando as três informações acima da pessoa.</li>
+                    </ol>
+                    <Pessoa nome="Nome da pessoa" sobrenome="Sobrenome da pessoa" idade="Idade da pesoa"/>
+                    <Pessoa nome="" sobrenome="" idade=""/>
+                    <Pessoa nome="Maria" sobrenome="Germano" idade="25"/>
+                    <Pessoa nome="João" sobrenome="Germano" idade="35"/>
+                    <Pessoa nome="Rafael" sobrenome="Germano" idade="45"/>
+                </article>
+                <article>
+                    <h2>Atividade guiada: componente elemento para tabela periódica</h2>
+                    <table>
+                        <caption>
+                            Série dos Lantanídeos
+                        </caption>
+                        <tr>
+                            <td>
+                                <ElementoQuimico nmr={57} simbulo="La" nome="Lantânio"/>
+                            </td>
+                            <td>
+                                <ElementoQuimico nmr={58} simbulo="Ce" nome="Cério"/>
+                            </td>
+                        </tr>
+                    </table>
                 </article>
             </section>
         </>
